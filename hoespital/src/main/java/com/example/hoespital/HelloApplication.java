0ad28bf47e0b40b2f3b3900e89cf654fc35ef2ad
Kaito <moonboyz999@gmail.com>
2024-09-09 03:59:24 +0800

    add comments

--- hoespital/src/main/java/com/example/hoespital/HelloApplication.java
+++ hoespital/src/main/java/com/example/hoespital/HelloApplication.java
@@ -1,12 +1,14 @@
 package com.example.hoespital;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
+//kai
+//Talha
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
