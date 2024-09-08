c33f2961c4ef7f5845ce9bb375c5d7eb50fd413a
Kaito <moonboyz999@gmail.com>
2024-09-09 04:11:44 +0800

    added .gitignore

--- hoespital/src/main/java/com/example/hoespital/HelloApplication.java
+++ hoespital/src/main/java/com/example/hoespital/HelloApplication.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 //kai
 //Talha
 //Danish
+//Rizz
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
